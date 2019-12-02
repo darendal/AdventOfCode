d3e840e1c22ddca9786faf772827ced79812f056
Brendan Ware <bware@mobileiron.com>
2019-12-02 15:20:17 -0500

    Initial commit

---
+++ AdventRunner/src/main/java/AdventRunner.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2019 MobileIron, Inc.
+ * All rights reserved.
+ */
+
+import solutions.AbstractSolution;
+
+public class AdventRunner {
+
+    public static void main(String[] args) {
+
+        AbstractSolution.test();
+        System.out.println("test");
+    }
+}
---
+++ CommonAdvent/src/main/java/solutions/AbstractSolution.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2019 MobileIron, Inc.
+ * All rights reserved.
+ */
+
+package solutions;
+
+public class AbstractSolution {
+
+    public static void test() {
+        System.out.println("success");
+    }
+
+}
