ffd043003cedfc4ffd6998d35b3a9245c3fdbaf7
Brendan Ware <bware@mobileiron.com>
2019-12-07 23:46:38 -0500

    Add time to SolutionList output

--- CommonAdvent/src/main/java/solutions/AbstractSolutionList.java
+++ CommonAdvent/src/main/java/solutions/AbstractSolutionList.java
@@ -17,11 +17,14 @@ public class AbstractSolutionList implements SolutionList {
 
     @Override
     public void runAll() {
+        long startTime = System.currentTimeMillis();
         solutionList.forEach(AbstractSolution::run);
+        System.out.println("\nTime taken : " + (System.currentTimeMillis() - startTime) + " millisecond(s).");
     }
 
     @Override
     public void runLast() {
+
         this.solutionList.get(solutionList.size() - 1).run();
     }
 
