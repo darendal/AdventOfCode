5227dad5ea29cc046c2d7156548c6ed280009e84
Brendan Ware <bware@mobileiron.com>
2020-12-04 11:36:16 -0500

    Day 4 solutions - modified to use all regex

--- AdventOfCode2020/src/main/java/solutions/day4/Solution2.java
+++ AdventOfCode2020/src/main/java/solutions/day4/Solution2.java
@@ -7,8 +7,6 @@ package solutions.day4;
 import java.util.List;
 import java.util.regex.Pattern;
 
-import utils.IntUtils;
-
 public class Solution2 extends Solution1 {
 
     public Solution2() {
@@ -42,6 +40,10 @@ public class Solution2 extends Solution1 {
 
     protected class Passport {
 
+        private final Pattern birthYearPattern = Pattern.compile("(19[2-9]\\d)|(200[0-2])");
+        private final Pattern issueYearPattern = Pattern.compile("(201\\d)|(2020)");
+        private final Pattern expireYearPattern = Pattern.compile("(202\\d)|(2030)");
+        private final Pattern heightPattern = Pattern.compile("(6\\din)|(59in)|(7[0-6]in)|(1[5-8]\\dcm)|(19[0-3]cm)");
         private final Pattern hairColorPattern = Pattern.compile("#[a-f,0-9]{6}");
         private final Pattern eyeColorPattern = Pattern.compile("(amb)|(blu)|(brn)|(gry)|(grn)|(hzl)|(oth)");
         private final Pattern pidPattern = Pattern.compile("[0-9]{9}");
@@ -60,66 +62,27 @@ public class Solution2 extends Solution1 {
 
             switch (key) {
                 case "byr":
-                    setBirthYear(values[1]);
+                    this.birthYear = birthYearPattern.matcher(values[1]).matches();
                     break;
                 case "iyr":
-                    setIssueYear(values[1]);
+                    this.issueYear = issueYearPattern.matcher(values[1]).matches();
                     break;
                 case "eyr":
-                    setExpireYear(values[1]);
+                    this.expireYear = expireYearPattern.matcher(values[1]).matches();
                     break;
                 case "hgt":
-                    setHeight(values[1]);
+                    this.height = heightPattern.matcher(values[1]).matches();
                     break;
                 case "ecl":
-                    this.setEyeColor(values[1]);
+                    this.eyeColor = this.eyeColorPattern.matcher(values[1]).matches();
                     break;
                 case "pid":
-                    this.setPassportId(values[1]);
+                    this.passportId = this.pidPattern.matcher(values[1]).matches();
                     break;
                 case "hcl":
-                   this.setHairColor(values[1]);
+                    this.hairColor = hairColorPattern.matcher(values[1]).matches();
                     break;
             }
-        }
-
-        private void setBirthYear(String param) {
-            int year = IntUtils.tryParse(param, 0);
-            this.birthYear = year >= 1920 && year <= 2002;
-        }
-
-        private void setIssueYear(String param) {
-            int year = IntUtils.tryParse(param, 0);
-            this.issueYear = year >= 2010 && year <= 2020;
-        }
-
-        private void setExpireYear(String param) {
-            int year = IntUtils.tryParse(param, 0);
-            this.expireYear = year >= 2020 && year <= 20130;
-        }
-
-        private void setHeight(String param) {
-            if (param.endsWith("cm")) {
-                int height = IntUtils.tryParse(param.substring(0, param.length() - 2), 0);
-                this.height = height >= 150 && height <= 193;
-            } else if (param.endsWith("in")) {
-                int height = IntUtils.tryParse(param.substring(0, param.length() - 2), 0);
-                this.height = height >= 59 && height <= 76;
-            } else {
-                this.height = false;
-            }
-        }
-
-        private void setEyeColor(String param) {
-            this.eyeColor = this.eyeColorPattern.matcher(param).matches();
-        }
-
-        private void setPassportId(String param) {
-            this.passportId = this.pidPattern.matcher(param).matches();
-        }
-
-        private void setHairColor(String param) {
-            this.hairColor = hairColorPattern.matcher(param).matches();
         }
 
         public boolean isValid() {
