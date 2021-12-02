0fb03abcb1a64506e16c3b9c4d3e668c1f17e4b4
Brendan Ware <bware43@gmail.com>
2021-12-02 10:10:13 -0800

    Upgrade to Python 3.10 and reformat day 2

--- Python/app/2022/2/code.py
+++ Python/app/2022/2/code.py
@@ -2,46 +2,49 @@ from common.utils import get_input
 
 
 def part_1():
-    data = [x.split(" ") for x in get_input()]
+    data = [x.split() for x in get_input()]
 
     depth = 0
     horizontal = 0
 
     for i in data:
 
-        direction: str = i[0]
-        amt: int = int(i[1])
+        direction, amt = i
+        amt: int = int(amt)
 
-        if direction.startswith('f'):
-            horizontal += amt
-        elif direction.startswith('d'):
-            depth -= amt
-        elif direction.startswith('u'):
-            depth += amt
+        match direction:
+            case 'forward':
+                horizontal += amt
+            case 'down':
+                depth -= amt
+            case 'up':
+                depth += amt
 
     print(abs(depth) * horizontal)
 
 
 def part_2():
-    data = [x.split(" ") for x in get_input()]
+    data = [x.split() for x in get_input()]
 
     depth = 0
     horizontal = 0
     aim = 0
 
     for i in data:
-        direction: str = i[0]
-        amt: int = int(i[1])
+        direction, amt = i
+        amt: int = int(amt)
 
-        if direction.startswith('f'):
-            horizontal += amt
-            depth += (aim * amt)
-        elif direction.startswith('d'):
-            aim -= amt
-        elif direction.startswith('u'):
-            aim += amt
+        match direction:
+            case 'forward':
+                horizontal += amt
+                depth += (aim * amt)
+            case 'down':
+                aim -= amt
+            case 'up':
+                aim += amt
 
     print(abs(depth) * horizontal)
+
 
 def main():
     part_1()
